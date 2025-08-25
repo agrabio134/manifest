bd0531b7fe74100faa8cb24996650e670bb8c0fb
agrabio134 <agrabioharvey@gmail.com>
2025-08-25 23:59:55 +0800

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="header" id="home">
-      <h1>Manifest Your Dreams</h1>
-      <p>Elevate Your Life with $MANIFEST Power</p>
+      <h1>Manifest Power, Elevate your Life</h1>
+      <p>The Power of Manifest Elevates Life</p>
       <a href="https://app.nexa.xyz/trade/0xc466c28d87b3d5cd34f3d5c088751532d71a38d93a8aae4551dd56272cfb4355::manifest::MANIFEST" target="_blank" rel="noopener noreferrer" className="join-btn">
         Join the Movement
       </a>
